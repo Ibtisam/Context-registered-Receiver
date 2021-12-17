f14a4ba050d3a1be95f86d10720d3f975c750902
Ibtisam <ibtisam.gull@gmail.com>
2021-12-17 11:44:17 +0500

    Updated

--- app/src/main/java/com/example/contextregisteredreceiver/MainActivity.java
+++ app/src/main/java/com/example/contextregisteredreceiver/MainActivity.java
@@ -12,14 +12,13 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     public static final String CUSTOM_ACTION = "com.example.contextregisteredreceiver.ACTION_SIMPLEST_BCAST";
     private MyBroadcastReceiver receiver;
-    private IntentFilter intentFilter;
     private LocalBroadcastManager mBroadcastMgr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intentFilter = new IntentFilter(CUSTOM_ACTION);
+        IntentFilter intentFilter = new IntentFilter(CUSTOM_ACTION);
         receiver = new MyBroadcastReceiver();
 
         mBroadcastMgr = LocalBroadcastManager.getInstance(getApplicationContext());
